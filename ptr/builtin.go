a8c3d43a13b60cb0f701749b667c10a3650eeac9
市川恭佑 (ebi) <github@mail.ebi-yade.com>
2022-04-03 01:34:35 +0900

    Include the generated artifact code

---
+++ ptr/builtin.go
@@ -0,0 +1,96 @@
+package ptr
+
+// MIT License
+// Copyright (c) 2022 市川恭佑 (ebi)
+// Although the generator command ebi-yade/pointable specifies the MIT License,
+// you are allowed to delete this license note if you want.
+
+// Bool returns a pointer to the given bool.
+func Bool(v bool) *bool {
+	return &v
+}
+
+// Uint8 returns a pointer to the given uint8.
+func Uint8(v uint8) *uint8 {
+	return &v
+}
+
+// Uint16 returns a pointer to the given uint16.
+func Uint16(v uint16) *uint16 {
+	return &v
+}
+
+// Uint32 returns a pointer to the given uint32.
+func Uint32(v uint32) *uint32 {
+	return &v
+}
+
+// Uint64 returns a pointer to the given uint64.
+func Uint64(v uint64) *uint64 {
+	return &v
+}
+
+// Int8 returns a pointer to the given int8.
+func Int8(v int8) *int8 {
+	return &v
+}
+
+// Int16 returns a pointer to the given int16.
+func Int16(v int16) *int16 {
+	return &v
+}
+
+// Int32 returns a pointer to the given int32.
+func Int32(v int32) *int32 {
+	return &v
+}
+
+// Int64 returns a pointer to the given int64.
+func Int64(v int64) *int64 {
+	return &v
+}
+
+// Float32 returns a pointer to the given float32.
+func Float32(v float32) *float32 {
+	return &v
+}
+
+// Float64 returns a pointer to the given float64.
+func Float64(v float64) *float64 {
+	return &v
+}
+
+// Complex64 returns a pointer to the given complex64.
+func Complex64(v complex64) *complex64 {
+	return &v
+}
+
+// Complex128 returns a pointer to the given complex128.
+func Complex128(v complex128) *complex128 {
+	return &v
+}
+
+// Byte returns a pointer to the given byte.
+func Byte(v byte) *byte {
+	return &v
+}
+
+// Rune returns a pointer to the given rune.
+func Rune(v rune) *rune {
+	return &v
+}
+
+// Uint returns a pointer to the given uint.
+func Uint(v uint) *uint {
+	return &v
+}
+
+// Int returns a pointer to the given int.
+func Int(v int) *int {
+	return &v
+}
+
+// String returns a pointer to the given string.
+func String(v string) *string {
+	return &v
+}
